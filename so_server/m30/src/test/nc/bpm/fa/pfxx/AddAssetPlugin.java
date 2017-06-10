c27e10453cff479ac1ea9437d37651a6d080279c
chaolong <chaol@iiot.ac.cn>
2017-06-10 14:31:39 +0800

    修改新增资产申请单插件逻辑

--- so_server/m30/src/test/nc/bpm/fa/pfxx/AddAssetPlugin.java
+++ so_server/m30/src/test/nc/bpm/fa/pfxx/AddAssetPlugin.java
@@ -6,10 +6,13 @@ import nc.bs.pfxx.plugin.AbstractPfxxPlugin;
 import nc.bs.xml.out.tool.XmlOutTool;
 import nc.impl.pfxx.plugins.AssetPfxxImpl;
 import nc.itf.fa.prv.INewAsset;
+import nc.itf.fa.service.INewAssetService;
+import nc.vo.am.proxy.AMProxy;
 import nc.vo.fa.newasset.AggNewAssetVO;
 import nc.vo.fa.newasset.NewAssetHeadVO;
 import nc.vo.pfxx.auxiliary.AggxsysregisterVO;
 import nc.vo.pub.BusinessException;
+import nc.vo.uif2.LoginContext;
 
 public class AddAssetPlugin extends AbstractPfxxPlugin{
 
@@ -17,7 +20,29 @@ public class AddAssetPlugin extends AbstractPfxxPlugin{
 	protected Object processBill(Object vo, ISwapContext swapContext,
 			AggxsysregisterVO aggvo) throws BusinessException {
 		// TODO 自动生成的方法存根
-		return new AssetPfxxImpl().processBill(vo, swapContext, aggvo);
+		/*
+		INewAsset service = AMProxy.lookup(INewAsset.class);
+		AggNewAssetVO temp = service.queryBillOfVOByPK("1001A41000000000DO00");
+		NewAssetHeadVO tempHead = (NewAssetHeadVO)(temp.getParent());
+		try{
+			AggNewAssetVO[] temps = new AggNewAssetVO[1];
+			temps[0] = temp;
+			XmlOutTool.votoXmlFile("bpm_add_asset", temps, tempHead.getPk_org(), tempHead.getBill_code());
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		return null;
+		*/
+		
+		AggNewAssetVO billVO = (AggNewAssetVO)vo;
+		//创建LoginContext
+		LoginContext loginContext = new LoginContext();
+		NewAssetHeadVO headVO = (NewAssetHeadVO)(billVO.getParent());
+		loginContext.setPk_group(headVO.getPk_group());
+		loginContext.setPk_org(headVO.getPk_org());
+		return AMProxy.lookup(INewAssetService.class).saveFromWeb(loginContext, billVO);
+		
+		
 	}
 
 }

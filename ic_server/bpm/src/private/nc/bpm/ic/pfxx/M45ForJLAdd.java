9e7d5befaa96a182579f20f3a3356a291b539711
chaolong <chaol@iiot.ac.cn>
2017-06-15 18:58:24 +0800

    计量系统根据到货单信息导入采购入库单插件

--- ic_server/bpm/src/private/nc/bpm/ic/pfxx/M45ForJLAdd.java
+++ ic_server/bpm/src/private/nc/bpm/ic/pfxx/M45ForJLAdd.java
@@ -2,6 +2,11 @@ package nc.bpm.ic.pfxx;
 
 import nc.bs.pfxx.ISwapContext;
 import nc.bs.pfxx.plugin.AbstractPfxxPlugin;
+import nc.bs.xml.out.tool.XmlOutTool;
+import nc.pubitf.ic.m45.api.IPurchaseInQueryAPI;
+import nc.vo.am.proxy.AMProxy;
+import nc.vo.ic.m45.entity.PurchaseInHeadVO;
+import nc.vo.ic.m45.entity.PurchaseInVO;
 import nc.vo.pfxx.auxiliary.AggxsysregisterVO;
 import nc.vo.pub.BusinessException;
 
@@ -16,6 +21,15 @@ public class M45ForJLAdd  extends AbstractPfxxPlugin{
 	protected Object processBill(Object vo, ISwapContext swapContext,
 			AggxsysregisterVO aggvo) throws BusinessException {
 		// TODO 自动生成的方法存根
+		IPurchaseInQueryAPI queryService = AMProxy.lookup(IPurchaseInQueryAPI.class);
+		PurchaseInVO [] exportBillVOs = queryService.queryVOByIDs(new String[]{"1001A410000000001GIX"});
+		PurchaseInHeadVO headVO =  (PurchaseInHeadVO)(exportBillVOs[0].getParentVO());
+		try {
+			XmlOutTool.votoXmlFile("JL_45_add", exportBillVOs, headVO.getPk_org(), headVO.getVbillcode());
+		} catch (Exception e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
 		return null;
 	}
 

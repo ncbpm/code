cfc8c5c089ccbf53871ca3b78b860f04143a52eb
chaolong <chaol@iiot.ac.cn>
2017-06-15 16:26:00 +0800

    不合格品处理状态会写 处理插件逻辑修改

--- qc_server/bpm/src/private/nc/bpm/qc/pfxx/RejectBillForBpmADD.java
+++ qc_server/bpm/src/private/nc/bpm/qc/pfxx/RejectBillForBpmADD.java
@@ -3,15 +3,20 @@ package nc.bpm.qc.pfxx;
 import nc.bs.pfxx.ISwapContext;
 import nc.bs.pfxx.plugin.AbstractPfxxPlugin;
 import nc.bs.xml.out.tool.XmlOutTool;
+import nc.itf.qc.c003.maintain.IReportMaintain;
 import nc.itf.qc.c004.maintain.IRejectMaintain;
 import nc.itf.qc.c004.page.IRejectBillMaintainApp;
 import nc.vo.am.proxy.AMProxy;
 import nc.vo.pfxx.auxiliary.AggxsysregisterVO;
 import nc.vo.pub.BusinessException;
 import nc.vo.pub.VOStatus;
+import nc.vo.pub.lang.UFDateTime;
+import nc.vo.qc.c003.entity.ReportHeaderVO;
+import nc.vo.qc.c003.entity.ReportVO;
 import nc.vo.qc.c004.entity.RejectBillHeadVO;
 import nc.vo.qc.c004.entity.RejectBillItemVO;
 import nc.vo.qc.c004.entity.RejectBillVO;
+import nc.vo.so.pub.enumeration.BillStatus;
 
 public class RejectBillForBpmADD extends AbstractPfxxPlugin{
 
@@ -31,6 +36,7 @@ public class RejectBillForBpmADD extends AbstractPfxxPlugin{
 			e.printStackTrace();
 		}
 		*/
+		final Integer PASSED = 3;
 		RejectBillVO billVO = (RejectBillVO)vo;
 		RejectBillHeadVO headVO = (RejectBillHeadVO)billVO.getParentVO();
 		
@@ -45,7 +51,9 @@ public class RejectBillForBpmADD extends AbstractPfxxPlugin{
 		temps[0] = queryService.queryMC004App(new String[]{headVO.getPk_rejectbill()})[0];	
 		RejectBillHeadVO tempHeadVO = (RejectBillHeadVO)temps[0].getParentVO();
 		//设置审批状态--通过
-		tempHeadVO.setFbillstatus(3);
+		tempHeadVO.setFbillstatus(PASSED);
+		tempHeadVO.setStatus(VOStatus.UPDATED);
+		//tempHeadVO.setModifiedtime(new UFDateTime());
 		//设置明细 判定结果
 		RejectBillItemVO[] modifyItems = (RejectBillItemVO[])billVO.getChildrenVO();
 		RejectBillItemVO[] originItems = (RejectBillItemVO[])temps[0].getChildrenVO();
@@ -62,7 +70,19 @@ public class RejectBillForBpmADD extends AbstractPfxxPlugin{
 		service.saveBase(temps, new Object(), originVOs);	
 		
 		//设置质检报告 审批状态
-		
+		 //根据不合格单的 pk_reportbill 查出质检报告
+		IReportMaintain reportService = AMProxy.lookup(IReportMaintain.class);
+		ReportVO orgReport = reportService.querySingleBillByPkAndBatchInfo(tempHeadVO.getPk_reportbill());
+		ReportHeaderVO reportHead = (ReportHeaderVO)orgReport.getParentVO();
+		reportHead.setFbillstatus(PASSED);
+		reportHead.setStatus(VOStatus.UPDATED);
+		//reportHead.setModifiedtime(new UFDateTime());
+		//update
+		ReportVO[] tempReports = new ReportVO[1];
+		tempReports[0] = orgReport;
+		ReportVO[] orgReports = new ReportVO[1];
+		orgReports[0] = reportService.querySingleBillByPkAndBatchInfo(tempHeadVO.getPk_reportbill());
+		reportService.saveBase(tempReports, new Object(), orgReports);
 		return billVO;
 	}
 

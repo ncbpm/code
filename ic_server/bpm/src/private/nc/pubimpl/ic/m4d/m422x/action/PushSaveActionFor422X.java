5b30d5e79fd9cc528e4d62f5f1a01940946c77e7
administrator <administrator@WIN-STFAVRRVIDF.dzf.com>
2017-06-22 17:40:24 +0800

    生成材料出库规则
    Change-Id: I4cbbdc12869687a7742dd2748d691aa920c49358
    Signed-off-by: administrator <administrator@WIN-STFAVRRVIDF.dzf.com>

--- ic_server/bpm/src/private/nc/pubimpl/ic/m4d/m422x/action/PushSaveActionFor422X.java
+++ ic_server/bpm/src/private/nc/pubimpl/ic/m4d/m422x/action/PushSaveActionFor422X.java
@@ -6,7 +6,6 @@ import nc.bs.ic.m4d.sign.SignBP;
 import nc.bs.ic.pub.base.ICAroundProcesser;
 import nc.impl.ic.general.GeneralPushSaveAction;
 import nc.impl.pubapp.pattern.rule.plugin.IPluginPoint;
-import nc.pubimpl.ic.m4d.m55aa.action.Push55AAto4DProcess;
 import nc.vo.ic.m4d.entity.MaterialOutVO;
 
 public class PushSaveActionFor422X extends GeneralPushSaveAction<MaterialOutVO> {
@@ -21,7 +20,7 @@ public class PushSaveActionFor422X extends GeneralPushSaveAction<MaterialOutVO> 
 
 	@Override
 	public void addBeforeRule(ICAroundProcesser<MaterialOutVO> processor) {
-		processor.addBeforeRule(new Push55AAto4DProcess());
+		processor.addBeforeRule(new Push422Xto4DProcess());
 		processor.addBeforeRule(new IgnoreSafetyStockCheck<MaterialOutVO>());
 	}
 

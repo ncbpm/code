31f2159b61fb92a1a8e38942100a6755ba60e180
ncbpm <1115460696@qq.com>
2017-07-30 15:46:30 +0800

    读卡器代码

---
+++ cardreader/src/nc/ss/card/CardInfo.java
@@ -0,0 +1,58 @@
+package nc.ss.card;
+
+public class CardInfo {
+        /// <summary>
+        /// 身份证号码
+        /// </summary>
+        public String CardNo;
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public String Name;
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public String Sex;
+        /// <summary>
+        /// 出生日期
+        /// </summary>
+        public String Birthday;
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public String Address;
+        /// <summary>
+        /// 追加地址
+        /// </summary>
+        public String AddressEx;
+        /// <summary>
+        /// 发卡机关
+        /// </summary>
+        public String Department;
+        /// <summary>
+        /// 证件开始日期
+        /// </summary>
+        public String StartDate;
+        /// <summary>
+        /// 证件结束日期
+        /// </summary>
+        public String EndDate;
+        /// <summary>
+        /// 民族
+        /// </summary>
+        public String Nation;
+        /// <summary>
+        /// 相片路径
+        /// </summary>
+        public String PhotoPath;
+        /// <summary>
+        /// 相片的字节信息
+        /// </summary>
+        public byte[] ArrPhotoByte;
+        /// <summary>
+        /// 时时图片字节信息
+        /// </summary>
+        public byte[] PhoTimeByte;
+        
+        
+}
